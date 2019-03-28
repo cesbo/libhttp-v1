0a93dd480d18fb5c35e9984f85654f33302078ea
Andrey Dyldin <and@cesbo.com>
2019-03-28 17:54:31 +0200

    initial commit

---
+++ tests/request.rs
@@ -0,0 +1,30 @@
+extern crate http;
+use http::Request;
+
+const TEST1: &str = "GET /path?query HTTP/1.1\r\n\
+    Host: 127.0.0.1:8000\r\n\
+    User-Agent: libhttp\r\n\
+    \r\n";
+
+#[test]
+fn test_reader_send() {
+    let mut request = Request::new();
+    request.init("GET", "http://127.0.0.1:8000/path?query");
+    request.set("User-Agent", "libhttp");
+
+    let mut dst: Vec<u8> = Vec::new();
+    request.send(&mut dst).unwrap();
+    assert_eq!(&dst, TEST1.as_bytes());
+}
+
+#[test]
+fn test_reader_read() {
+    request = Request::new();
+    request.read(TEST1.as_bytes()).unwrap();
+
+    assert_eq!(request.get_method(), "GET");
+    assert_eq!(request.get_path(), "/path?query");
+    assert_eq!(request.headers.get("host"), "127.0.0.1:8000");
+    assert_eq!(request.headers.get("user-agent"), "libhttp");
+}
+
