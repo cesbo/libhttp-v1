bdd6b9c279cf06b57104ed75425a7d6ea865846c
Maxim Vinogradov <maxvinza@yandex.ru>
2019-04-02 22:25:36 +0300

    changed url, added some parametrs

--- src/url.rs
+++ src/url.rs
@@ -3,31 +3,65 @@ use std::io::BufRead;
 
 #[derive(Default)]
 pub struct Url {
+    scheme: String,
     name: String,
     path: String,
+    query: String,
+    fragment: String,
+}
+
+
+fn url_path(v: &Vec<&str>) -> String {
+    let mut flag: usize = 5;
+    let mut result = String::new();
+    for part in v {
+        if flag > 1 {
+            flag -= 1;
+        }
+        if flag == 1 {
+            result.push_str(&format!("/{}", part));
+        }
+    }
+    result
 }
 
 
 impl Url {
     pub fn new(inp: &str) -> Self {
-        let mut url = Url::default();
-        let v: Vec<&str> = inp.split('/').collect();
-        url.name=v[2].to_string();
-        url.path=url.find_path(&v);
-        url
+        let path: Vec<&str> =  inp.split('?').collect();
+        let query_fragment: Vec<&str> = path[1].split('#').collect();
+        let v: Vec<&str> = path[0].split('/').collect();
+        Url {
+            scheme: v[0].replace(":",""),
+            name: v[2].to_string(),
+            path: url_path(&v),
+            query: query_fragment[0].to_string(),
+            fragment: query_fragment[1].to_string(),
+        }
     }
     
-    fn find_path(&self, v: &Vec<&str>) -> String {
-        let mut flag: usize = 5;
-        let mut result = String::new();
-        for part in v {
-            if flag > 1 {
-                flag -= 1;
-            }
-            if flag == 1 {
-                result.push_str(&format!("/{}", part));
-            }
-        }
-        result
+    #[inline]
+    pub fn get_name(&self) -> &str {
+        self.name.as_str()
+    }
+    
+    #[inline]
+    pub fn get_scheme(&self) -> &str {
+        self.scheme.as_str()
+    }
+    
+    #[inline]
+    pub fn get_path(&self) -> &str {
+        self.path.as_str()
+    }
+    
+    #[inline]
+    pub fn get_query(&self) -> &str {
+        self.query.as_str()
+    }
+    
+    #[inline]
+    pub fn get_fragment(&self) -> &str {
+        self.fragment.as_str()
     }
 }
--- tests/url.rs
+++ tests/url.rs
@@ -4,9 +4,9 @@ use http::url::Url;
 #[test]
 fn test_url() {
     let mut url = Url::new("http://127.0.0.1:8000/path?query#fragment");
-    /*assert_eq!(url.scheme, "http");
-    assert_eq!(url.name, "127.0.0.1:8000");
-    assert_eq!(url.path, "/path");
-    assert_eq!(url.query, "query");
-    assert_eq!(url.fragment, "fragment");*/
+    assert_eq!(url.get_scheme(), "http");
+    assert_eq!(url.get_name(), "127.0.0.1:8000");
+    assert_eq!(url.get_path(), "/path");
+    assert_eq!(url.get_query(), "query");
+    assert_eq!(url.get_fragment(), "fragment");
 }
