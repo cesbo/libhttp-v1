95d617e750019825345a6867bc4eccb0b7c0182e
Maxim Vinogradov <maxvinza@yandex.ru>
2019-04-10 18:21:33 +0300

    add some response functions

--- src/response.rs
+++ src/response.rs
@@ -13,17 +13,46 @@ use crate::error::{
 
 
 pub struct Response {
-    method: String,
+    version: String,
+    code: usize,
 }
 
 
 impl Response {
     pub fn new() -> Self {
         Response {
-            method: String::new(),
+            version: String::new(),
+            code: 0,
         }
     }
     pub fn read<R: Read>(&mut self, head: R) -> Result<()> {
+        let mut line = 0;
+        let mut reader = BufReader::new(head);
+        let mut buffer = String::new();
+        loop {
+            buffer.clear();
+            match reader.read_line(&mut buffer) {
+                Ok(v) => if v == 0 { break },
+                Err(e) => return Err(Error::from(e)),
+            };
+            if line == 0 {
+                let mut v = buffer.split(' ');
+                self.version += v.next().unwrap_or("");
+                self.code = (v.next().unwrap_or("")).parse::<usize>().unwrap_or(0);;
+            }
+            /*if line > 0 {
+                header::pars_heades_line(&mut self.headers, &buffer);
+            }*/
+            line += 1;
+        }
         Ok(())
+    }
+    
+    pub fn get_version(&self) -> &str {
+        self.version.as_str()
+    }
+    
+    pub fn get_code(&self) -> &usize {
+        &(self.code)
     }
 }
--- tests/response.rs
+++ tests/response.rs
@@ -7,5 +7,9 @@ const TEST1: &str = "HTTP/1.1 200 Ok\r\n\
     
 #[test]
 fn response_read() {
+    let code200: usize = 200;
     let mut response = Response::new();
+    response.read(TEST1.as_bytes()).unwrap();
+    assert_eq!(response.get_version(), "HTTP/1.1");
+    assert_eq!(response.get_code(), &code200);
 }
