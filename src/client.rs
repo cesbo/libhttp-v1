ec2e7e55110d6735e6e946f6f59eeb20852e528e
Maxim Vinogradov <maxvinza@yandex.ru>
2019-04-25 23:48:24 +0300

    added write and read for client

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,8 @@ use std::net::TcpStream;
 use std::io::{
     BufWriter,
     BufReader,
+    Write,
+    Read,
 };
 
 use crate::request::Request;
@@ -11,12 +13,39 @@ use crate::error::{
     Result,
 };
 
+
 #[derive(Default)]
 pub struct HttpClient {
     pub response: Response,
     pub request: Request,
     stream: Option<TcpStream>,
+    stream_read: Option<BufReader<TcpStream>>,
+    stream_write: Option<BufWriter<TcpStream>>,
 }
+
+
+impl Write for HttpClient {
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        match self.stream {
+            Some(v) => v.write(buf),
+            _ =>  Err(Error::Custom("socket not ready")),
+        }
+    }
+}
+
+
+impl Read for HttpClient {
+    #[inline]
+    fn read_to_string(&mut self, buf: &[u8]) -> String {
+        let buffer = match self.stream {
+            Some(v) => self.response.read(buf),
+            _ =>  Err(Error::Custom("socket not ready")),
+        };
+        buffer.to_string()
+    }
+}
+
 
 impl HttpClient {
     pub fn new() -> Self {
@@ -32,14 +61,13 @@ impl HttpClient {
                     "https" => 443,
                     _ => return Err(Error::Custom("HttpClient: port not defined for unknown scheme")),
                 }
-            }
+            } 
             v => v,
         };
         let mut stream = TcpStream::connect((host, port))?;
-        let stream_clone = stream.try_clone()?;
         {
-            let mut writer = BufWriter::new(&mut stream);
-            self.request.send(&mut writer)?;
+            self.stream_write = BufWriter::new(&mut stream);
+            self.request.send(&mut self.stream_write)?;
         }
         self.response.parse(&stream)?;
         self.stream = Some(stream);
