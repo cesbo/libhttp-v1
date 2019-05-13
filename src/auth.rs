71f16422912ba55ea9d55829d76844caf1ee87f6
Maxim Vinogradov <maxvinza@yandex.ru>
2019-05-13 17:30:36 +0300

    write diges auth

--- src/auth.rs
+++ src/auth.rs
@@ -1,5 +1,9 @@
-use base64::encode;
 use std::collections::HashMap;
+use openssl::hash::{
+    hash, 
+    MessageDigest,
+};
+use base64::encode;
 
 use crate::request::Request;
 use crate::response::Response;
@@ -10,13 +14,16 @@ pub fn basic(request: &mut Request) {
 }
 
 
-pub fn digest(response: &mut Response) {
-    let params = get_digest_params(&response);
-}
-
-
-fn get_digest_params(response: &Response) -> HashMap<String, String> {
-    let mut ret = HashMap::new();
+pub fn digest(response: &mut Response, stream: &mut stream, request: &mut Request) {
+    let qop: i8;
+    let realm: str;
+    let nonce: str;
+    let nonce_count = "00000001"; // TODO - function to calculate
+    let client_nonce = "0a4f113b"; // TODO - function to calculate
+    let uri = request.url.get_path();
+    let mut i = request.url.get_prefix().splitn(2, ':');
+    let username = i.next().unwrap("");
+    let password = i.next().unwrap("");
     let mut header = match response.get_header("www-authenticate") {
         Some(v) => v.as_str(),
         _ => return ret,
@@ -28,7 +35,47 @@ fn get_digest_params(response: &Response) -> HashMap<String, String> {
             continue;
         }
         let value = i.next().unwrap_or("");
-        ret.insert(key.trim().to_string(), value.trim().to_string()); 
+        match key.trim().to_lowercase(){
+            "qop" => {
+                qop = match value.trim().to_lowercase() {
+                    "auth" => 1,
+                    "auth-int" => 2,
+                    "auth,auth-int" => 3,
+                    _ => 0,
+                }
+            },
+            "realm" => realm = value,
+            "nonce" => nonce = value,
+        }        
     }
-    ret
+    let a1 = format!("{}:{}:{}", username, realm, password);
+    let ha1 = hash_md5(a1);
+    let a2 = match qop {
+        1 => format("{}:{}", request.get_method(), uri),
+        2 => format("{}:{}:{}", request.get_method(), uri, entity_body_md5(stream)),
+        3 => format("{}:{}", request.get_method(), uri),
+        _ => "",
+    }
+    let ha2 = hash_md5(a2);
+    let response = match { 
+        1, 3 => format("{}:{}:{}:{}:auth:{}", ha1, nonce, nonce_count, client_nonce, ha2);
+        2 => format("{}:{}:{}:{}:auth-int:{}", ha1, nonce, nonce_count, client_nonce, ha2);
+        _ => format("{}:{}:{}", ha1, nonce, ha2);
+    }
+    let hresponse = hash_md5(response);
+
+    let authorization_head = b"Digest "; 
+    write(authorization_head, "username=\"{}\"", username);
+    //TODO other headlines 
+    request.set("authorization", authorization_head);
+}
+
+fn entity_body_md5(stream: &mut stream) -> String { 
+    let mut body = String::new();
+    stream.read_to_string(&mut body).unwrap();
+    hash_md5(body)
+}
+
+fn hash_md5(s: str) -> String {  // TODO - fix return type if it need
+    hash(MessageDigest::md5(), s).unwrap()
 }
