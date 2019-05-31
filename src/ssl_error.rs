56590760377252acd1bc1e3b882611ed0c50089d
Andrey Dyldin <and@cesbo.com>
2019-06-01 02:35:56 +0500

    missing file

---
+++ src/ssl_error.rs
@@ -0,0 +1,81 @@
+use std::{
+    error,
+    fmt,
+    net::TcpStream,
+};
+
+
+#[derive(Debug)]
+pub struct SslError(openssl::error::ErrorStack);
+
+
+impl error::Error for SslError {
+    #[inline]
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> { Some(&self.0) }
+}
+
+
+impl fmt::Display for SslError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = self.0.errors().get(0)
+            .and_then(openssl::error::Error::reason)
+            .unwrap_or("");
+        write!(f, "{}", s)
+    }
+}
+
+
+impl From<openssl::error::ErrorStack> for SslError {
+    #[inline]
+    fn from(e: openssl::error::ErrorStack) -> SslError { SslError(e) }
+}
+
+
+#[derive(Debug)]
+pub struct HandshakeError(openssl::ssl::HandshakeError<TcpStream>);
+
+
+impl error::Error for HandshakeError {
+    #[inline]
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> { Some(&self.0) }
+}
+
+
+impl fmt::Display for HandshakeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self.0 {
+            openssl::ssl::HandshakeError::SetupFailure(ee) => {
+                let s = ee.errors().get(0)
+                    .and_then(openssl::error::Error::reason)
+                    .unwrap_or("");
+                write!(f, "{}", s)
+            }
+            openssl::ssl::HandshakeError::Failure(ee) => {
+                let inner_error = ee.error();
+                if let Some(io_ee) = inner_error.io_error() {
+                    write!(f, "{}", io_ee)
+                } else if let Some(ssl_ee) = inner_error.ssl_error() {
+                    let s = ssl_ee.errors().get(0)
+                        .and_then(openssl::error::Error::reason)
+                        .unwrap_or("unknown");
+
+                    let v = ee.ssl().verify_result();
+                    if v.as_raw() == 0 {
+                        write!(f, "{}", s)
+                    } else {
+                        write!(f, "{}: {}", s, v.error_string())
+                    }
+                } else {
+                    unreachable!()
+                }
+            }
+            _ => unimplemented!(),
+        }
+    }
+}
+
+
+impl From<openssl::ssl::HandshakeError<TcpStream>> for HandshakeError {
+    #[inline]
+    fn from(e: openssl::ssl::HandshakeError<TcpStream>) -> HandshakeError { HandshakeError(e) }
+}
