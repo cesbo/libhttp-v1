1d1605da05c1672a6c02114b443c2d6c59f192b3
Andrey Dyldin <and@cesbo.com>
2019-07-11 16:53:52 +0500

    cleaning

--- src/version.rs
+++ src/version.rs
@@ -1,6 +1,11 @@
 use std::fmt;
 
 
+const HTTP10: &str = "HTTP/1.0";
+const HTTP11: &str = "HTTP/1.1";
+const RTSP10: &str = "RTSP/1.0";
+
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum HttpVersion {
     HTTP10,
@@ -18,9 +23,9 @@ impl Default for HttpVersion {
 impl fmt::Display for HttpVersion {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            HttpVersion::HTTP11 => fmt::Write::write_str(f, "HTTP/1.1"),
-            HttpVersion::HTTP10 => fmt::Write::write_str(f, "HTTP/1.0"),
-            HttpVersion::RTSP10 => fmt::Write::write_str(f, "RTSP/1.0"),
+            HttpVersion::HTTP11 => fmt::Write::write_str(f, HTTP11),
+            HttpVersion::HTTP10 => fmt::Write::write_str(f, HTTP10),
+            HttpVersion::RTSP10 => fmt::Write::write_str(f, RTSP10),
         }
     }
 }
@@ -28,9 +33,9 @@ impl fmt::Display for HttpVersion {
 
 impl From<&str> for HttpVersion {
     fn from(s: &str) -> HttpVersion {
-        if s.eq_ignore_ascii_case("http/1.1") {
+        if s.eq_ignore_ascii_case(HTTP11) {
             HttpVersion::HTTP11
-        } else if s.eq_ignore_ascii_case("rtsp/1.0") {
+        } else if s.eq_ignore_ascii_case(RTSP10) {
             HttpVersion::RTSP10
         } else {
             HttpVersion::HTTP10
