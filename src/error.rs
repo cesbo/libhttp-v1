5c790d70b8a3f9128ea4b4f51c17ec0e9e0b3b9c
Andrey Dyldin <and@cesbo.com>
2020-06-03 22:10:32 +0300

    pest grammar for key-value

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 
+/// Breaks function and returns Err
 #[macro_export]
 macro_rules! bail {
     ($e:expr) => {
@@ -13,6 +14,7 @@ macro_rules! bail {
 }
 
 
+/// Breaks function and returns Err if conditional expression evaluates false
 #[macro_export]
 macro_rules! ensure {
     ($cond:expr, $e:expr) => {
